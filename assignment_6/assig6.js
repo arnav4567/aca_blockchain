8abf596db8239ea38ff5254741bc397af44701a6
Arnav Gupta <arnavg20@iitk.ac.in>
2021-07-19 00:18:24 +0530

    Create assig6.js

---
+++ assignment_6/assig6.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+const port = 8787;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get('/',(req,res)=>res.send('Post some data @ endpoint /hash to get its cryptographic hash')); //not required, just for clarity
+app.get('/hash',(req,res)=>res.send('Nothing to \'get\' here ! You may post some data.'));
+
+app.post('/hash', function(req,res){
+    console.log("Data is : ",JSON.stringify(req.body));
+    var hash = crypto.createHash('sha256').update(req.body.data).digest('hex');
+    hash = hash.toString();
+    res.json({'hash':hash});
+    console.log('Successful !');
+    console.log(JSON.stringify({"hash":hash}),'\n');
+})
+
+app.listen(port, ()=>{
+    console.log('Server is at localhost:'+port);
+})
+
+
+
